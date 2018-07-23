59c9fb68d0c1f900010d322bc3e3ab3184172eb1
Joseph Caruso <joseph@phriidev.com>
2018-07-23 17:54:36 -0500

    Add Electron boilerplate

---
+++ main.js
@@ -0,0 +1,30 @@
+const {app, BrowserWindow} = require('electron');
+
+// Prevent main window from being garbage-collected
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', function () {
+    // Allow window to be garbage-collected
+    mainWindow = null;
+  });
+
+  mainWindow.setFullScreen(true);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
